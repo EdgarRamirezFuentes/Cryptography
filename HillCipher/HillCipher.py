d0e933b2a04d5da1156d908508104e2ca18a79b6
Edgar Alejandro Ramírez Fuentes <50359137+EdgarRamirezFuentes@users.noreply.github.com>
2021-09-18 20:48:38 -0500

    First commit Hill Cipher

---
+++ HillCipher/HillCipher.py
@@ -0,0 +1,16 @@
+import math
+
+class HillCipher:
+    def __init__(self, alphabet):
+        '''
+            Hill Cipher
+
+            Parameters
+            ------------------------
+            alphabet : list
+                It is the alphabet used for encryption/decryption in the Hill Cipher
+        '''
+        self.__alphabet = alphabet
+    
+    
+
